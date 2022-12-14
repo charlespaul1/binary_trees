31ffaaa94d53467fc38a25e1d339722bda92cb08
charlespaul1 <charlespaulwabomba@gmail.com>
2022-12-14 17:05:15 +0300

    creating a binary tree node

---
+++ binary_trees.h
@@ -0,0 +1,43 @@
+#ifndef BINARY_T
+#define BINARY_T
+
+#include <stdlib.h>
+
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+/**
+ * struct queue_s - Structure for queue
+ * @child: Integer stored in the node
+ * @next: Pointer to the next node
+ */
+struct queue_s
+{
+	binary_tree_t *child;
+	struct queue_s *next;
+};
+typedef struct queue_s queue_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+
+#endif
